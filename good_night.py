c433f7ca22a42917bb23fe8fed144f18116ab331
Angelo <44776259+whsyourdaddy@users.noreply.github.com>
2020-02-14 17:27:24 +0800

    Update good_night.py

--- good_night.py
+++ good_night.py
@@ -1,22 +1,26 @@
 import requests
 import json
-from twilio.rest import Client
 import time
+from twilio.rest import Client
 
-account_sid = "AC360aff664a181761414840b4f1754324"
-auth_token = "b336fc6efc02ee7a5d377f2aa3eab818"
-url = 'http://api.tianapi.com/txapi/wanan/index?key=4a611d178bf4a853f0e6c754efdb8fe3'
+account_sid = Your account_sid  # account_sid=aaaaaaaaaaaaaaaa
+auth_token = Your auth_token    # auth_token=aaaaaaaaaaaaaa
+url = 'http://api.tianapi.com/txapi/wanan/index?key=Your key'   # key=aaaaaaaaaaaaaaa
 
+#获取信息内容
 page = requests.get(url=url)
 content = json.loads(page.text)['newslist'][0]['content']
-client = Client(account_sid, auth_token)
 
+#发送信息
+client = Client(account_sid, auth_token)
 message = client.messages.create(
-    to="+8618827189301",
-    from_="+12566079294",
+    to="Your phone number", # to="+86123456"
+    from_="server number",  #from_="+12123456"
     body=str(content))
+
+# 输出日志
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+print(content)
 print(message.sid)
-print(message.status)
 
 
